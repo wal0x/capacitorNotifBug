3c4e1552e61a20d193431491aad6df054d31c062
wal0x <h.medwail@gmail.com>
2020-12-06 21:46:17 +0100

    Bug local notification

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Plugins } from '@capacitor/core';
+
+const { LocalNotifications } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
+  async fire() {
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: "Title",
+          body: "Body",
+          id: 1,
+          schedule: { at: new Date(Date.now() + 1000 * 5) },
+          sound: null,
+          attachments: null,
+          actionTypeId: "",
+          extra: null
+        }
+      ]
+    });
+
+  }
 }
